a60a25e4c39ad8264ab98e63282a0923c0554212
Ilia Riabko <ilia@Pechka02.>
2025-03-05 23:04:51 +0100

    Add multiple cubes generation

--- 02_simple3d/scene_definition.hpp
+++ 02_simple3d/scene_definition.hpp
@@ -14,22 +14,38 @@
 
 inline SimpleScene createCubeScene(MaterialFactory &aMaterialFactory, GeometryFactory &aGeometryFactory) {
 	SimpleScene scene;
-	auto cube = std::make_shared<Cube>();
+	//auto cube1 = std::make_shared<Cube>();
+	//cube1->setName("CUBE");
+	//cube1->addMaterial(
+	//	"solid",
+	//	MaterialParameters(
+	//		"solid_color",
+	//		RenderStyle::Solid,
+	//		{
+	//			{ "u_solidColor", glm::vec4(1.0f, 0.5f, 0.0f, 1.0f)}
+	//		})
+	//	);
+	//cube1->prepareRenderData(aMaterialFactory, aGeometryFactory);
+	//cube1->setRotation(glm::quat(glm::vec3(0.0f, glm::radians(45.0f), 0.0f)));
+	//scene.addObject(cube1);
 
-	cube->setName("CUBE");
-	cube->addMaterial(
-		"solid",
-		MaterialParameters(
-			"solid_color",
-			RenderStyle::Solid,
-			{
-				{ "u_solidColor", glm::vec4(1.0f, 0.5f, 0.0f, 1.0f)}
-			})
-		);
-	cube->prepareRenderData(aMaterialFactory, aGeometryFactory);
+	int cubeCount = 20;
+	for (int i = 1; i < cubeCount + 1; i++) {
+		for (int j = 1; j < cubeCount + 1; j++) {
+			Cube cube;
+			cube.setName("Cube" + i + ':' + j);
+			cube.setPosition(glm::uvec3(i - 1, j - 1, (float)cubeCount));
+			cube.addMaterial(
+				"solid",
+				MaterialParameters(
+					"solid_color",
+					RenderStyle::Solid,{
+						{ "u_solidColor", glm::vec4(1.0f / i, 1.0f / j, 0.0f, 1.0f)}
+					}));
+			cube.prepareRenderData(aMaterialFactory, aGeometryFactory);
+			scene.addObject(std::make_shared<Cube>(cube));
+		}
+	}
 
-	// cube->setRotation(glm::quat(glm::vec3(0.0f, glm::radians(45.0f), 0.0f)));
-	scene.addObject(cube);
 	return scene;
 }
-
